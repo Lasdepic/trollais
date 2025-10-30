14998248011908d0bcb91e0525b67f6a782ecbef
Lasdepic <clementjordan34@gmail.com>
2025-10-30 12:18:49 +0100

    Intégration de ollama dans le projet

---
+++ server/app.js
@@ -0,0 +1,28 @@
+import express from "express";
+import ollama from "ollama";
+
+const app = express();
+app.use(express.json());
+
+const PORT = 3000;
+
+app.post("/api/ask", async (req, res) => {
+    const { question } = req.body;
+  try {
+    const response = await ollama.chat({
+      model: "trollais",
+      messages: [
+        {
+          role: "user",
+          content: question,
+        },
+      ],
+    });
+    const answer = response.message.content;
+    res.status(200).json(answer);
+  } catch (error) {
+    res.status(500).json({error : error, message : "Une Erreur est survenue sur le serveur"})
+  }
+});
+
+app.listen(PORT, () => console.log("Le serveur ok sur le port" + PORT))
